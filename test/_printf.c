07e2e1e7b41fb59f9292f960c6aa4da4aaeac651
Blessingobulor <blessingpeters24@gmail.com>
2023-01-25 19:37:20 -0800

    task0

--- test/_printf.c
+++ test/_printf.c
@@ -4,38 +4,38 @@
 
 int _printf(const char *format, ...);
 {
-	int i = 0, j;
-	char *new_str = NULL;
+int i = 0, j;
+        char *new_str = NULL;
 
-	va_list args;
+        va_list args;
 
-	va_start(args, format);
+        va_start(args, format);
 
-	while (format[i] != '\0')
-	{
-		if (format[i] != %)
-			_putchar(format[i]);
-		else
-		{
-			if (format[i + 1] == 'c')
-			{
-				_putchar(va_args(args, int));
-				i++;
-			}
-			else if (format[i + 1] == 's')
-			{
-				i++;
-				new_str = va_arg(args, char *);
-				j = 0;
-				while (new_str[j] != '\0')
-				{
-					_putchar(new_str[j]);
-					j++
-				}
-			
-			}
-		}
-	}
-	va_end(args);
-	return (0);
+        while (format[i] != '\0')
+        {
+        if (format[i] != %)
+
+                _putchar(format[i]);
+        else
+        {
+        if (format[i + 1] == 'c')
+{
+        _putchar(va_args(args, int));
+        i++;
+        }
+        else if (format[i + 1] == 's')
+        {
+        i++;
+        new_str = va_arg(args, char *);
+        j = 0;
+        while (new_str[j] != '\0')
+        {
+        _putchar(new_str[j]);
+        j++
+        }
+
+        }
 }
+        }
+        va_end(arg);
+	return (0);
